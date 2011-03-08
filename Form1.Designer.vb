3eadef1af9be2e888e9ab1d4de44cb1b34c1a587
Cesar Quinteros <cquinterox@gmail.com>
2011-03-07 23:38:30 -0500

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.fbdSource = New System.Windows.Forms.FolderBrowserDialog()
+        Me.btnSource = New System.Windows.Forms.Button()
+        Me.btnDestination = New System.Windows.Forms.Button()
+        Me.btnCopy = New System.Windows.Forms.Button()
+        Me.txtExtension = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.fbdDestination = New System.Windows.Forms.FolderBrowserDialog()
+        Me.chkKeepDirStruc = New System.Windows.Forms.CheckBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.SuspendLayout()
+        '
+        'fbdSource
+        '
+        Me.fbdSource.ShowNewFolderButton = False
+        '
+        'btnSource
+        '
+        Me.btnSource.Location = New System.Drawing.Point(17, 12)
+        Me.btnSource.Name = "btnSource"
+        Me.btnSource.Size = New System.Drawing.Size(85, 31)
+        Me.btnSource.TabIndex = 0
+        Me.btnSource.Text = "Source"
+        Me.btnSource.UseVisualStyleBackColor = True
+        '
+        'btnDestination
+        '
+        Me.btnDestination.Location = New System.Drawing.Point(108, 12)
+        Me.btnDestination.Name = "btnDestination"
+        Me.btnDestination.Size = New System.Drawing.Size(85, 31)
+        Me.btnDestination.TabIndex = 1
+        Me.btnDestination.Text = "Destination"
+        Me.btnDestination.UseVisualStyleBackColor = True
+        '
+        'btnCopy
+        '
+        Me.btnCopy.Location = New System.Drawing.Point(17, 112)
+        Me.btnCopy.Name = "btnCopy"
+        Me.btnCopy.Size = New System.Drawing.Size(173, 31)
+        Me.btnCopy.TabIndex = 4
+        Me.btnCopy.Text = "Copy"
+        Me.btnCopy.UseVisualStyleBackColor = True
+        '
+        'txtExtension
+        '
+        Me.txtExtension.Location = New System.Drawing.Point(115, 54)
+        Me.txtExtension.Name = "txtExtension"
+        Me.txtExtension.Size = New System.Drawing.Size(73, 20)
+        Me.txtExtension.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(14, 57)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(97, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Extension to copy: "
+        '
+        'chkKeepDirStruc
+        '
+        Me.chkKeepDirStruc.AutoSize = True
+        Me.chkKeepDirStruc.Location = New System.Drawing.Point(19, 84)
+        Me.chkKeepDirStruc.Name = "chkKeepDirStruc"
+        Me.chkKeepDirStruc.Size = New System.Drawing.Size(138, 17)
+        Me.chkKeepDirStruc.TabIndex = 8
+        Me.chkKeepDirStruc.Text = "Keep directory structure"
+        Me.chkKeepDirStruc.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline
+        Me.LinkLabel1.LinkColor = System.Drawing.Color.Gray
+        Me.LinkLabel1.Location = New System.Drawing.Point(99, 159)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(92, 13)
+        Me.LinkLabel1.TabIndex = 9
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "C. Quinteros 2011"
+        Me.LinkLabel1.VisitedLinkColor = System.Drawing.Color.Gray
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(209, 187)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.chkKeepDirStruc)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtExtension)
+        Me.Controls.Add(Me.btnCopy)
+        Me.Controls.Add(Me.btnDestination)
+        Me.Controls.Add(Me.btnSource)
+        Me.MaximumSize = New System.Drawing.Size(225, 225)
+        Me.MinimumSize = New System.Drawing.Size(225, 225)
+        Me.Name = "frmMain"
+        Me.Text = "File Funnel "
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents fbdSource As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents btnSource As System.Windows.Forms.Button
+    Friend WithEvents btnDestination As System.Windows.Forms.Button
+    Friend WithEvents btnCopy As System.Windows.Forms.Button
+    Friend WithEvents txtExtension As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents fbdDestination As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents chkKeepDirStruc As System.Windows.Forms.CheckBox
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,113 @@
+﻿Imports System.IO
+Imports System.Threading
+
+Public Class frmMain
+    Dim intCopyCount As Integer 'Count of files copied
+    Dim strExtension As String
+
+    Dim objWriter As New StreamWriter(Now.ToString("yyyy-MM-dd--hh-mm-ss") & "_log.txt") 'Log
+
+    Private Sub btnSource_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSource.Click
+        fbdSource.ShowDialog()
+    End Sub
+
+    Private Sub btnDestination_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDestination.Click
+        fbdDestination.ShowDialog()
+    End Sub
+
+    Private Sub btnCopy_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCopy.Click
+        Try
+            Dim appThread As New Thread(New ThreadStart(AddressOf init)) 'For threading purposes. Tell the threading object which function. Must not include parameters : (
+
+            If (btnCopy.Text = "Copy") Then
+                If (inputsValid()) Then
+                    enableControls(False)
+                    appThread.Start() ' start threaded process!
+                Else
+                    MsgBox("Please make sure the source, destination, and extension are set correctly.", MsgBoxStyle.Critical, "Parameters Missing")
+                End If
+            ElseIf (btnCopy.Text = "Cancel") Then
+                enableControls(True)
+                appThread.Abort() ' stop the thread process. 
+                End
+            End If
+        Catch ex As Exception
+            ' future error logging 
+        End Try
+    End Sub
+    Public Function enableControls(ByVal bool As Boolean)
+        Select Case bool
+            Case True
+                btnSource.Enabled = True
+                btnDestination.Enabled = True
+                txtExtension.Enabled = True
+                btnCopy.Text = "Copy"
+            Case False
+                btnSource.Enabled = False
+                btnDestination.Enabled = False
+                txtExtension.Enabled = False
+                btnCopy.Text = "Cancel"
+        End Select
+        Return True
+    End Function
+    Public Function init()
+
+        Dim dirSRC As New DirectoryInfo(fbdSource.SelectedPath) 'set source
+        Dim dirDEST As New DirectoryInfo(fbdDestination.SelectedPath) 'set destination
+        copyTargetFilesInDir(dirSRC, dirDEST, strExtension) 'copyTargetFilesInDir(source, destination, extension)
+        objWriter.Close() 'stop logging when done
+        MessageBox.Show("Done! [" & intCopyCount & "] files copied") ' Done message
+        End
+        Return True
+    End Function
+
+    Public Function inputsValid()
+        'Set up before hand for ease of validation
+        Dim regEx As New System.Text.RegularExpressions.Regex("^\.\S+$")
+        strExtension = txtExtension.Text
+
+        'Make sure selected paths are set and the extension is in extension format
+        If Not (fbdSource.SelectedPath = String.Empty) And Not (fbdDestination.SelectedPath = String.Empty) And regEx.IsMatch(strExtension) Then
+            Return True 'validates
+        Else
+            Return False 'doesn't validate
+        End If
+    End Function
+
+    Public Function copyTargetFilesInDir(ByVal src_dir As DirectoryInfo, ByVal dest_dir As DirectoryInfo, ByVal ext As String)
+        'Dim objWriter As New StreamWriter("file_list.txt") // THIS MUST BE RUN OUTSIDE RECURSION FUNCTION
+        Dim strDir As String() = Directory.GetFileSystemEntries(src_dir.FullName)
+        Dim strEntry As String 'Generic entry for comparing filenames
+        Dim strTempString As String ' For logging purposes primarily
+
+        For Each strEntry In strDir 'For each directory
+            Dim dirInner As New DirectoryInfo(strEntry)
+            If (dirInner.Exists) Then ' If what we found is a directory then...do formula again in search for files
+                If (chkKeepDirStruc.Checked) Then ' If the user wants to keep the directory structure
+                    Dim newDest As New DirectoryInfo(dest_dir.FullName & dirInner.FullName.ToString.Replace(src_dir.FullName, ""))
+                    System.IO.Directory.CreateDirectory(newDest.FullName)
+                    copyTargetFilesInDir(dirInner, newDest, ext)
+                Else
+                    copyTargetFilesInDir(dirInner, dest_dir, ext)
+                End If
+            End If
+            Dim file As New FileInfo(strEntry) 'ITS'S A FILE!
+            'Set the location for the new file by changing it's original path for the new path
+            Dim newFile As New FileInfo(dest_dir.FullName & file.FullName.ToString.Replace(src_dir.FullName, ""))
+            'Check to see the file on hand is the type we were looking for then copy, count, log
+            If (file.Exists And file.Extension = ext And Not newFile.Exists) Then ' extension with period and makes sure new copy does not exist
+                System.IO.File.Copy(file.FullName, dest_dir.FullName & file.FullName.ToString.Replace(src_dir.FullName, ""))
+                intCopyCount += 1 'For file count
+                strTempString = file.FullName & " >> " & newFile.FullName 'For logging primarily
+                objWriter.WriteLine(intCopyCount & ". " & strTempString) 'Log copy
+            End If
+        Next
+        'objWriter.Close() // THIS MUST BE RUN OUTSIDE RECURSION FUNCTION
+        Return True
+    End Function
+
+    Private Sub LinkLabel1_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        System.Diagnostics.Process.Start("http://atikik.tumblr.com/")
+    End Sub
+End Class
+
